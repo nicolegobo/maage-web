ea653cda0e944e6383bb513777388539d5612db2
Chris Thomas <ccthomas@anl.gov>
2025-02-20 06:55:13 -0600

    pre-release(core): site-wide modifications to styles, markup, templates, configs and build tools

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* Home. */
@@ -8,9 +8,9 @@ router.get("/", function (req, res) {
 });
 
 /* About MAAGE */
-router.get("/about-maage", function (req, res) {
+router.get("/about", function (req, res) {
   req.applicationModule = "p3/app/p3app";
-  res.render("pages/about-maage", { title: "PATRIC", request: req, response: res });
+  res.render("pages/about", { title: "PATRIC", request: req, response: res });
 });
 
 /* Advisory Boards. */
@@ -44,75 +44,115 @@ router.get("/announcements", function (req, res) {
 });
 
 /* Citations */
-router.get('/citation', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/citation', { title: 'PATRIC', request: req, response: res });
+router.get("/citation", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/citation", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET publications page. */
-router.get('/publications', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/publications', { title: 'PATRIC', request: req, response: res });
+router.get("/publications", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/publications", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET related resources page. */
-router.get('/related-resources', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/related-resources', { title: 'PATRIC', request: req, response: res });
+router.get("/related-resources", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/related-resources", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* Team Members */
-router.get('/team-members', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/team-members', { title: 'PATRIC', request: req, response: res });
+router.get("/team", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/team", { title: "PATRIC", request: req, response: res });
 });
 
 /* SARS-CoV-2 */
-router.get('/sars-cov-2', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/sars-cov-2', { title: 'PATRIC', request: req, response: res });
+router.get("/sars-cov-2", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/sars-cov-2", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* SARS-CoV-2 User Guide */
-router.get('/docs/user-guides/sars-cov-2', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('docs/user-guides/sars-cov-2', { title: 'PATRIC', request: req, response: res });
+router.get("/docs/user-guides/sars-cov-2", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("docs/user-guides/sars-cov-2", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* SARS-CoV-2 Tutorial */
-router.get('/docs/tutorials/sars-cov-2', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('docs/tutorials/sars-cov-2', { title: 'PATRIC', request: req, response: res });
+router.get("/docs/tutorials/sars-cov-2", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("docs/tutorials/sars-cov-2", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET outreach page. */
-router.get('/brc-calendar', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/brc-calendar', { title: 'PATRIC', request: req, response: res });
+router.get("/brc-calendar", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/brc-calendar", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET all searches page. */
-router.get('/searches', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/searches', { title: 'PATRIC', request: req, response: res });
+router.get("/searches", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/searches", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET all tools & services page. */
-router.get('/tools', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/tools', { title: 'PATRIC', request: req, response: res });
+router.get("/tools", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/tools", { title: "PATRIC", request: req, response: res });
 });
 
 /* GET privacy policy page. */
-router.get('/privacy-policy', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/privacy-policy', { title: 'PATRIC', request: req, response: res });
+router.get("/privacy-policy", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/privacy-policy", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 /* GET privacy policy page. */
-router.get('/verify_failure', function (req, res) {
-  req.applicationModule = 'p3/app/p3app';
-  res.render('pages/verify_failure', { title: 'PATRIC', request: req, response: res });
+router.get("/verify_failure", function (req, res) {
+  req.applicationModule = "p3/app/p3app";
+  res.render("pages/verify_failure", {
+    title: "PATRIC",
+    request: req,
+    response: res,
+  });
 });
 
 module.exports = router;
